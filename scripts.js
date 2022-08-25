55ec715047cd5244420d202dd30dfd70f5dcd985
Renan-Araujo-Gomes <renanaraujo.go@gmail.com>
2022-08-25 16:10:48 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function calcule(){    
+    let real = document.getElementById('input-real').value
+    let dolar = document.getElementById('input-dolar').value
+    let resultado = real / dolar
+    alert('O resultado é ' + resultado)
+}
